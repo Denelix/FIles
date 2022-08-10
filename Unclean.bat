3b830564f9684b7df3299d27817f769b7ef4bed9
Denelix <69168805+Denelix@users.noreply.github.com>
2022-08-10 00:39:08 -0500

    Update Unclean.bat

--- Unclean.bat
+++ Unclean.bat
@@ -1,3 +1,3 @@
 cd C:\Program Files\NetBalancer
-nbcmd.exe settings priorities edit "C:\Users\Dene\.lunarclient\jre\4dcd188552ce8876d5e55e1f6d22505109bfa4cb\zulu17.34.19-ca-jre17.0.3-win_x64\bin\javaw.exe" Delayed High 0 120 false
+nbcmd.exe settings priorities edit "C:\Users\Dene\.lunarclient\jre\4dcd188552ce8876d5e55e1f6d22505109bfa4cb\zulu17.34.19-ca-jre17.0.3-win_x64\bin\javaw.exe" Delayed Delayed 0 120 false
 exit
